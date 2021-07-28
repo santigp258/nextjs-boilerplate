d6f1f59ee82596f8feb4da6e0896801aa6c9bc8b
Santigp258 <santigp258@gmail.com>
2021-07-28 10:06:16 -0500

    next-js-boilerplate-ts done

--- system.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-System.config({
-	'paths': {
-		'@/components/*' : './src/components/*',
-		'@/pages/*' : './src/pages/*',
-		'@/style/*' : './src/style/*',
-		'@/store/*' : './src/store/*',
-	}
-});
